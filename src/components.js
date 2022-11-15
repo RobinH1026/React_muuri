13733e813f4d04a570cb6fc6ee4e1e25d59b5cc3
RobinH1026 <andreyvanishevf@gmail.com>
2022-11-16 01:19:20 +0900

    complete side bar

--- src/components.js
+++ src/components.js
@@ -2,19 +2,23 @@ import React from "react";
 
 // Demo component.
 export const Demo = ({ children }) => {
-  return <section className="grid-demo">{children}</section>;
+    return(
+    <section className="grid-demo">
+        {children}
+    </section>
+    )
 };
 
 // Header component.
 export const Header = () => (
-  <React.Fragment>
-    <h2 className="section-title">
-      <span>Grid Demo</span>
-    </h2>
-    <h4 className="section-title">
-      <div>
-        <h4>Resize the window to change the number of columns</h4>
-      </div>
-    </h4>
-  </React.Fragment>
+    <React.Fragment>
+        <div className="header">
+            <div className="noti_div">
+                <img alt="gas pump icon" src="https://ultrasound.money/_next/static/media/gas-slateus.08e75f71.svg" width="14px"/>&nbsp;
+                <p><span className="text-orange-400">20</span> Gwei</p> &nbsp;&nbsp;&nbsp;
+                <img alt="Ethereum Ether icon" src="https://ultrasound.money/_next/static/media/eth-slateus.4ac61d8c.svg" width="14px" />
+                <p><span className="text-white">1,218</span> USD <span className="text-green-400">(+1.1%)</span></p>
+            </div>
+        </div>
+    </React.Fragment>
 );
--- src/index.js
+++ src/index.js
@@ -5,45 +5,47 @@ import ReactDom from "react-dom";
 import { MuuriComponent, getResponsiveStyle } from "muuri-react";
 import { useMediaQuery } from "react-responsive";
 /* Utils & components */
-import { generateItems, ThemeContext } from "./utils";
+import { ThemeContext } from "./utils";
 import { Header, Demo } from "./components";
+import SideBar from "./SideBar";
 /* Style */
 import "./style.css";
+import 'antd/dist/antd.css';
+import Crypto from "./assets/images/crypto.png"
+import Img1 from "./assets/images/img.png"
 
 // App.
 const App = () => {
-  // Items state.
-  const [items] = useState(generateItems());
   const items1 = [
     {
       id: 1,
       color: "green",
-      title: "sm"
+      title: "sm1"
     },
     {
       id: 2,
       color: "green",
-      title: "sm"
+      title: "sm2"
     },
     {
       id: 3,
       color: "green",
-      title: "sm"
+      title: "sm3"
     },
     {
       id: 4,
       color: "green",
-      title: "sm"
+      title: "sm4"
     },
     {
       id: 5,
       color: "green",
-      title: "sm"
+      title: "sm5"
     },
     {
       id: 6,
       color: "green",
-      title: "sm"
+      title: "sm6"
     }
   ];
 
@@ -51,61 +53,95 @@ const App = () => {
     {
       id: 7,
       color: "blue",
-      title: "lg"
+      title: "lg1-1",
+      header: "Watchlist",
+      show: true
     },
     {
       id: 8,
       color: "blue",
-      title: "lg"
+      title: "lg2-2",
+      header: "Tweets",
+      show: true
     },
     {
       id: 9,
       color: "blue",
-      title: "lg"
+      title: "lg3-3",
+      header: "Ethereum Chart",
+      show: true
     },
     {
       id: 10,
       color: "blue",
-      title: "lg"
+      title: "lg4-4",
+      header: "New Coins",
+      show: true
     }
   ];
+  const [items, setItems] = useState(items2);
+  const children1 = items1.map((props) => {
+    return(
+      <Item key={props.id} {...props}>
+      </Item>
+    )
+  });
+  const children2 = items.filter(item=>item.show).map((props) => <Item key={props.id} {...props} />);
 
-  // Items to children.
-  const children1 = items1.map((props) => <Item key={props.id} {...props} />);
-  const children2 = items2.map((props) => <Item key={props.id} {...props} />);
+  const toggleItem = ({ id }) => {
+    const newItems = items.map((item, index) => {
+      if (id === item.id) {
+        return {
+          ...item,
+          show: !item.show
+        };
+      }
+      return { ...item }
+    }
+    );
+    setItems(newItems);
+  }
 
   return (
-    <Demo>
+    <div>
       <Header />
-      <ThemeProvider1>
-        <MuuriComponent
-          dragEnabled
-          dragFixed
-          dragSortPredicate={{
-            action: "swap"
-          }}
-          dragSortHeuristics={{
-            sortInterval: 0
-          }}
-        >
-          {children1}
-        </MuuriComponent>
-      </ThemeProvider1>
-      <ThemeProvider2>
-        <MuuriComponent
-          dragEnabled
-          dragFixed
-          dragSortPredicate={{
-            action: "swap"
-          }}
-          dragSortHeuristics={{
-            sortInterval: 0
-          }}
-        >
-          {children2}
-        </MuuriComponent>
-      </ThemeProvider2>
-    </Demo>
+      <div className="home">
+        <SideBar items={items} toggleItem={toggleItem} />
+        <div className="contain_div">
+        <Demo>
+          <ThemeProvider1>
+            <MuuriComponent
+              dragEnabled
+              dragFixed
+              dragSortPredicate={{
+                action: "swap"
+              }}
+              dragSortHeuristics={{
+                sortInterval: 0
+              }}
+            >
+              {children1}
+            </MuuriComponent>
+          </ThemeProvider1>
+          <ThemeProvider2>
+            <MuuriComponent
+              dragEnabled
+              dragFixed
+              dragSortPredicate={{
+                action: "swap"
+              }}
+              dragSortHeuristics={{
+                sortInterval: 0
+              }}
+            >
+              {children2}
+            </MuuriComponent>
+          </ThemeProvider2>
+        </Demo>
+        </div>
+      </div>
+    </div>
+    
   );
 };
 
@@ -124,11 +160,11 @@ const ThemeProvider1 = ({ children }) => {
     });
   }, [isBigScreen]);
 
-  
-
   return (
     <div className="sm_div">
-      <ThemeContext.Provider value={style}>{children}</ThemeContext.Provider>
+      <ThemeContext.Provider value={style}>
+         {children}
+      </ThemeContext.Provider>
     </div>
   );
 };
@@ -146,8 +182,6 @@ const ThemeProvider2 = ({ children }) => {
     });
   }, [isBigScreen]);
 
-  
-
   return (
     <div>
       <ThemeContext.Provider value={style}>{children}</ThemeContext.Provider>
@@ -156,13 +190,38 @@ const ThemeProvider2 = ({ children }) => {
 };
 
 // Item component.
-const Item = ({ color, title }) => {
+const Item = ({ color, title, id }) => {
   // The style concerns only the "dimensions" and "margins" of the items.
   const style = useContext(ThemeContext);
 
   return (
     <div style={style}>
-      <div className="item-content">{title}</div>
+      <div className={id<7?"item-content":"item-content lg_box"}>
+        {id === 1?
+          <div>
+            <div className="address">
+              <img src={Crypto} alt="crypto" className="crypto" />
+              <p>0x359…1a476</p>
+            </div>
+            <p className="arbitrage"><img src={Img1} alt="Img1" width="13px" /> &nbsp; Arbitrage</p>
+            <div className="row_div" style={{ marginTop:'10px' }}>
+              <div className="col_4"><p>Profit:</p></div>
+              <div className="col_8"><p className="div_p">$556,621.18</p></div>
+            </div>
+            <div className="row_div">
+              <div className="col_4"><p>Cost:</p></div>
+              <div className="col_8">$6,219.00</div>
+            </div>
+            <div className="row_div">
+              <div className="col_4"><p>ROI:</p></div>
+              <div className="col_8">$8,958.61</div>
+            </div>
+          </div>
+        :
+        <p></p>
+      }
+        {/* {title} */}
+      </div>
     </div>
   );
 };
